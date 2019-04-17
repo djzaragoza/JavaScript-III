5e0d52c2213bab0bf6833d547791746fae91ca1e
DJ Zaragoza <zaragoza0126@gmail.com>
2019-04-16 19:54:33 -0700

    first commit of JS III project for this.js file

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding - Whenever a function is in the global scope, the value of this inside of that function is the window object
+* 2. Whenever a function is called by a dot, the object before the dot is this keyword
+* 3. Whenever a constructor function is used, this refers to the specific instance of the object that was created and returned by the constructor function
+* 4. Whenever JavaScript's call or apply method is used, this is explicitly defined
 *
 * write out a code example of each explanation above
 */
